ef6becf12cd00a3973b832b0cfbd20c0c2f6ed18
programming6131 <kimutainashon@gmail.com>
2024-08-02 14:53:37 +0300

    first commit

---
+++ blockchain.go
@@ -0,0 +1,17 @@
+package main
+
+import "time"
+
+type Block struct {
+	data         map[string]any
+	hash         string
+	previoushash string
+	timestamp    time.Time
+	pow          int
+}
+
+type BlockChain struct {
+	genesisBlock Block
+	chain        []Block
+	difficulty   int
+}
