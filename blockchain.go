7c3807fcb5110feca0292f27dda97dc85177d979
programming6131 <kimutainashon@gmail.com>
2024-08-09 23:38:18 +0300

    calculate hash

--- blockchain.go
+++ blockchain.go
@@ -1,11 +1,17 @@
 package main
 
-import "time"
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+)
 
 type Block struct {
 	data         map[string]any
 	hash         string
-	previoushash string
+	previousHash string
 	timestamp    time.Time
 	pow          int
 }
@@ -14,4 +20,11 @@ type BlockChain struct {
 	genesisBlock Block
 	chain        []Block
 	difficulty   int
+}
+
+func (b Block) CalculateHash() string {
+	data, _ := json.Marshal(b.data)
+	blockData := b.previousHash + string(data) + b.timestamp.String() + strconv.Itoa(b.pow)
+	blockHash := sha256.Sum256([]byte(blockData))
+	return fmt.Sprintf("%x", blockHash)
 }
